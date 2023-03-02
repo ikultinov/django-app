6b1ed8331d3c4f90627d6c730cfb143e9c6de8a6
Ilya Kultinov <kultinov.i@gmail.com>
2023-03-03 00:50:13 +0300

    Import csv to Django project

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    image = models.URLField()
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('phone_detail', args=[str(self.name)])
